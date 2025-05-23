4cccd250fc6e74982d14f20d3c02598029a5e1c3
amir <amirtaki623@gmail.com>
2025-05-23 21:12:19 +0330

    crossHeader addEventListener

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 let topHeader = document.querySelector('.top')
 let headerUpper = document.getElementById('header-upper')
+let crossHeader = document.querySelector(".cross")
+
 
 topHeader.addEventListener("click", (event)=> {
     topHeader.classList.add("deactive")
     headerUpper.className = 'active'
+})
+
+crossHeader.addEventListener("click", (event)=> {
+    topHeader.classList.remove("deactive")
+    headerUpper.className = 'deactive'
 })
