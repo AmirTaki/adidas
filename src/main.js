b7fbb56473396cbf9ea1c669749bbef416185e5d
amir <amirtaki623@gmail.com>
2025-05-23 21:29:27 +0330

    js : edit querySelectro

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ let crossHeader = document.querySelector(".cross")
 
 topHeader.addEventListener("click", (event)=> {
     topHeader.classList.add("deactive")
-    headerUpper.className = 'active'
+    
 })
 
 crossHeader.addEventListener("click", (event)=> {
     topHeader.classList.remove("deactive")
-    headerUpper.className = 'deactive'
+
 })
