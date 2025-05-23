b98950f9a8f48262ef99b8bf65247cb23b7fbbb6
amir <amirtaki623@gmail.com>
2025-05-23 21:46:37 +0330

    js:edit animation & styles animation

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 let topHeader = document.querySelector('.top')
 let headerUpper = document.getElementById('header-upper')
-let crossHeader = document.querySelector(".cross")
+let crossHeader = document.querySelector(".cross i")
 
 
 topHeader.addEventListener("click", (event)=> {
     topHeader.classList.add("deactive")
+    headerUpper.classList.remove('heaer-downer')
+    headerUpper.classList.add('header-upper')
     
 })
 
 crossHeader.addEventListener("click", (event)=> {
     topHeader.classList.remove("deactive")
-
+    headerUpper.classList.remove('header-upper')
+    headerUpper.classList.add('heaer-downer')
+    console.log(headerUpper.classList)
 })
